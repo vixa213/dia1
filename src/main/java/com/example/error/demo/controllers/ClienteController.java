a48fc939b4ecc9a1ac80505aea502704781854fd
vixa213 <varlqw@gmail.com>
2021-03-19 21:35:09 -0600

    ver v4

--- src/main/java/com/example/error/demo/controllers/ClienteController.java
+++ src/main/java/com/example/error/demo/controllers/ClienteController.java
@@ -1,17 +1,25 @@
 package com.example.error.demo.controllers;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
+import java.util.UUID;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -35,6 +43,28 @@ public class ClienteController {
 
 	@Autowired
 	private IClienteService clienteService;
+
+	private final Logger log = LoggerFactory.getLogger(getClass());
+
+	@GetMapping(value = "/uploads/{filename:.+}")
+	public ResponseEntity<Resource> verFoto(@PathVariable String filename) {
+		Path pathFoto = Paths.get("uploads").resolve(filename).toAbsolutePath();
+		log.info("pathFoto: " + pathFoto);
+
+		Resource recurso = null;
+		try {
+			recurso = new UrlResource(pathFoto.toUri());
+			if (!recurso.exists() || !recurso.isReadable()) {
+				throw new RuntimeException("No se puede cargar la imagen: " + pathFoto.toString());
+			}
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return ResponseEntity.ok()
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + recurso.getFilename() + "\"")
+				.body(recurso);
+	}
 
 	@GetMapping(value = "/ver/{id}")
 	public String ver(@PathVariable(value = "id") Long id, Map<String, Object> model) {
@@ -91,17 +121,22 @@ public class ClienteController {
 		}
 
 		if (!foto.isEmpty()) {
-			
-			String rootPath = "C://Temp//uploads";
-			try {
-				byte[] bytes = foto.getBytes();
-				Path rutaCompleta = Paths.get(rootPath + "//" + foto.getOriginalFilename());
-				Files.write(rutaCompleta, bytes);
 
+			String uniqueFileName = UUID.randomUUID().toString() + "_" + foto.getOriginalFilename();
+			Path roothPath = Paths.get("uploads").resolve(foto.getOriginalFilename());
+			
+			Path roothAbsolutPath = roothPath.toAbsolutePath();
+			
+			log.info("roothPath:" + roothPath);
+			log.info("roothAbsolutPath:" + roothAbsolutPath);
+			
+			try {
+				Files.copy(foto.getInputStream(), roothAbsolutPath);
 				cliente.setFoto(foto.getOriginalFilename());
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+			Path rootPath = Paths.get("uploads").resolve(uniqueFileName);
 		}
 
 		clienteService.save(cliente);
